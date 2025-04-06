75c4def4d245145f1c58e3c467e0c5abd1b29e91
LeviTorresx <levist42@gmail.com>
2025-04-05 19:51:12 -0500

    base of landing page [002]

---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,13 @@
+
+export default function Landing() {
+  return (
+    <div className="grid">
+      <div className="item item-0">0</div>
+      <div className="item item-1">1</div>
+      <div className="item item-2">2</div>
+      <div className="item item-3">3</div>
+      <div className="item item-5 desktop-only">5</div>
+      <div className="item item-4 ">4</div>
+    </div>
+  )
+}
